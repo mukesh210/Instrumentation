b322e0158948a1d189b1249412348d0a9e760d68
Mukesh S. Prajapati <mukeshsprajapati210@gmail.com>
2018-09-03 22:10:42 +0530

    adding remaining files

---
+++ src/com/fetchingsize/Sizeof.java
@@ -0,0 +1,15 @@
+package com.fetchingsize;
+
+import java.lang.instrument.Instrumentation;
+
+final public class Sizeof {
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long sizeof(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
